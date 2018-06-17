1447169f06b5e19afe83b92cde62a47303e43700
mullerger84 <mullerger84@gmail.com>
2018-06-18 00:31:28 +0900

    1315 1316 프로젝트

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print('hello world')
+import matplotlib.pyplot as plt
+plt.figure(figsize = (5,5),dpi = 3000)
+import csv
+f = open('서울시 종로구 도로명 주소 정보.csv')
+
+data = csv.reader(f)
+a,e = input('구주소를 입력해주세요. ex) 서울특별시 종로구 후암동 143-8 :').split('-')
+a = str(a)
+e = str(e)
+a,b,c,d = a.split(' ')
+
+for row in data :
+    if a in row[0] :
+        if b in row[1] :
+            if c in row[2] :
+                if d in row[3] :
+                    if e in row[4] :
+                        print('신주소는', row[0]+' '+row[1]+' '+row[5]+' '+row[6]+'-'+row[7],'입니다.')
+                        plt.rc('font',family='Malgun Gothic')
+                        plt.plot(row[10],row[11],'.')
+                        plt.xlim(190000,210000)
+                        plt.ylim(450000,460000)
+                        plt.xlabel('x좌표')
+                        plt.ylabel('y좌표')
+                        plt.show()
